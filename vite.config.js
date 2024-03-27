f3f2ec079fda7ac961ce028bd14fae828c401726
anggernuramin <anggern514@gmail.com>
2024-03-27 09:32:44 +0700

    feature: create sidebar

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+// import dotenv from "dotenv";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  define: {
+    define: {
+      // Menetapkan semua variabel lingkungan ke dalam konfigurasi define
+      "process.env": process.env,
+    },
+  },
+});
