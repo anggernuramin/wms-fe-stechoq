e3d09e8d63b68afa9c866dffa123f6df3fb3abd1
vayndem <abrorhaikal@gmail.com>
2024-05-15 05:31:30 +0700

    improvement: barang keluar (80%)

---
+++ src/services/barangkeluar.services.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+
+export const GetBarangKeluar = async (teks, id) => {
+  try {
+    const res = await axios.get(
+      `${import.meta.env.VITE_VUE_APP_BASE_URL}/barangKeluar?search=${teks}&page=${id.value}`,
+      {
+        headers: {
+          'Cache-Control': 'no-cache' // Mengatur header Cache-Control untuk memastikan data selalu diambil dari server
+        }
+      }
+    )
+    return res.data.data
+  } catch (error) {
+    return error.message
+  }
+}
+
+// export const GetBarangKeluarbyID = async (teks) => {
+//     try {
+//       const res = await axios.get(`${import.meta.env.VITE_VUE_APP_BASE_URL}/barangKeluar?search=${teks}`, {
+//         headers: {
+//           'Cache-Control': 'no-cache' // Mengatur header Cache-Control untuk memastikan data selalu diambil dari server
+//         }
+//       })
+//       return res.data.data
+//     } catch (error) {
+//       return error.message
+//     }
+//   }
+
+export const DeleteBarangKeluar = async (id) => {
+  try {
+    const res = await axios.delete(`${import.meta.env.VITE_VUE_APP_BASE_URL}/barangKeluar/delete/${id}`)
+    console.log('🚀 ~ deleteBarangKeluar ~ res:', res)
+    return res.data.statusCode
+  } catch (error) {
+    return error.message
+  }
+}
+
+export const UpdateBarangkeluar = async (id) => {
+  try {
+    const res = await axios.patch(`${import.meta.env.VITE_VUE_APP_BASE_URL}/barangKeluar/update/${id}`)
+    console.log('🚀 ~ deleteBarangKeluar ~ res:', res)
+    return res.data.statusCode
+  } catch (error) {
+    return error.message
+  }
+}
